4fc9f90637446a1538a6a5f71acb4378dd4e6d9b
duddits212 <duddits7@gmail.com>
2013-04-17 01:39:36 +0700

    Rejected commit: Default

---
+++ app/yii.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Администратор
+ * Date: 20.03.13
+ * Time: 0:08
+ * To change this template use File | Settings | File Templates.
+ */
+class Yii extends YiiBase
+{
+    /**
+     * Returns the application singleton or null if the singleton has not been created yet.
+     * @return CWebApplication the application singleton, null if the singleton has not been created yet.
+     */
+    public static function app()
+    {
+        return self::app();
+    }
+}
